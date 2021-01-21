6ced53f453f50eb0a2f0a6545c099d0b84e06b2a
permadiekapermana <permadiekapermana@gmail.com>
2021-01-21 15:56:21 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args){
+		Shape s = new Shape();
+		s = new Rectangle();
+		s.draw();
+		s = new Circle();
+		s.draw();
+		s = new Triangle();
+		s.draw();
+	}
+}
